cf007dc94d3ceb5ea77d73820a61ce917394613e
Armando Luiz Bretas Neto <armando.neto@gazin.com.br>
2020-04-04 00:58:39 -0300

    Todo List Add Item

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,8 @@ class App extends StatelessWidget {
 
 class HomePage extends StatefulWidget {
   var items = new List<Item>();
-  HomePage(){
-    items=[];
+  HomePage() {
+    items = [];
     items.add(Item(title: 'Banana', done: true));
     items.add(Item(title: 'Abacate', done: true));
     items.add(Item(title: 'Laranja', done: false));
@@ -28,11 +28,36 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var newTaskCtrl = TextEditingController();
+
+  void add() {
+    
+    if(newTaskCtrl.text.isEmpty) return;
+
+    setState(() {
+      widget.items.add(Item(title: newTaskCtrl.text, done: false));
+      newTaskCtrl.clear();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Todo List"),
+        title: TextFormField(
+          controller: newTaskCtrl,
+          keyboardType: TextInputType.text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+          ),
+          decoration: InputDecoration(
+            labelText: "Nova tarefa",
+            labelStyle: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
       ),
       body: ListView.builder(
         itemCount: widget.items.length,
@@ -42,13 +67,18 @@ class _HomePageState extends State<HomePage> {
             title: Text(item.title),
             key: Key(item.title),
             value: item.done,
-            onChanged: (value){
-              setState((){
-                item.done= value;
+            onChanged: (value) {
+              setState(() {
+                item.done = value;
               });
             },
           );
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: add,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.blue,
       ),
     );
   }
