08dc68e919dd9ed7839c417bcc323bcdd33566d5
Armando Luiz Bretas Neto <armando.neto@gazin.com.br>
2020-04-04 00:15:59 -0300

    Hello Word

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(App());
 
-
-
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: HomePage()
-    );
+        title: 'Flutter App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: HomePage());
   }
 }
- 
- class HomePage extends StatelessWidget {
-   @override
-   Widget build(BuildContext context) {
-     return Scaffold(
-       body: Container(
-         child: Text("Hello Word")
-       )
-     );
-   }
- }
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: Container(child: Center(child: Text("Hello Word"))));
+  }
+}
