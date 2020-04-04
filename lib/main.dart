52392fb95879a1ac661074ea2ee0bc5c44cb8f8a
Armando Luiz Bretas Neto <armando.neto@gazin.com.br>
2020-04-04 01:24:37 -0300

    Todo List finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'models/item.dart';
 
 void main() => runApp(App());
@@ -18,9 +21,9 @@ class HomePage extends StatefulWidget {
   var items = new List<Item>();
   HomePage() {
     items = [];
-    items.add(Item(title: 'Banana', done: true));
-    items.add(Item(title: 'Abacate', done: true));
-    items.add(Item(title: 'Laranja', done: false));
+    // items.add(Item(title: 'Banana', done: true));
+    // items.add(Item(title: 'Abacate', done: true));
+    // items.add(Item(title: 'Laranja', done: false));
   }
 
   @override
@@ -31,13 +34,42 @@ class _HomePageState extends State<HomePage> {
   var newTaskCtrl = TextEditingController();
 
   void add() {
-    
-    if(newTaskCtrl.text.isEmpty) return;
+    if (newTaskCtrl.text.isEmpty) return;
 
     setState(() {
       widget.items.add(Item(title: newTaskCtrl.text, done: false));
       newTaskCtrl.clear();
+      save();
     });
+  }
+
+  void remove(int index){
+    setState((){
+      widget.items.removeAt(index);
+      save();
+    });
+  }
+
+  Future load() async{
+    var prefs = await SharedPreferences.getInstance();
+    var data = prefs.getString('data');
+
+    if(data != null) {
+      Iterable decoded = jsonDecode(data);
+      List<Item> result = decoded.map((x)  => Item.fromJson(x)).toList();
+      setState((){
+        widget.items = result;
+      });
+    }
+  }
+
+  save() async {
+    var prefs = await SharedPreferences.getInstance();
+    await prefs.setString('data', jsonEncode(widget.items));
+  }
+
+  _HomePageState(){
+    load();
   }
 
   @override
@@ -63,14 +95,24 @@ class _HomePageState extends State<HomePage> {
         itemCount: widget.items.length,
         itemBuilder: (BuildContext ctxt, int index) {
           final item = widget.items[index];
-          return CheckboxListTile(
-            title: Text(item.title),
+          return Dismissible(
+            child: CheckboxListTile(
+              title: Text(item.title),
+              value: item.done,
+              onChanged: (value) {
+                setState(() {
+                  item.done = value;
+                  save();
+                });
+              },
+            ),
             key: Key(item.title),
-            value: item.done,
-            onChanged: (value) {
-              setState(() {
-                item.done = value;
-              });
+            background: Container(
+              color: Colors.red.withOpacity(0.3),
+              child: Text("Excluir"),
+            ),
+            onDismissed: (direction){
+              remove(index);
             },
           );
         },
